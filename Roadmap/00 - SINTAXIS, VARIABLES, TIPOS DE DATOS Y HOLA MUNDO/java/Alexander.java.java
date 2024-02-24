503290998cf5f6d6485db5a893e23d63345e98af
AlexanderFrancis87 <67815900+AlexanderFrancis87@users.noreply.github.com>
2024-02-23 23:13:10 -0500

    Add files via upload

---
+++ Roadmap/00 - SINTAXIS, VARIABLES, TIPOS DE DATOS Y HOLA MUNDO/java/Alexander.java.java
@@ -0,0 +1,38 @@
+/*
+ *#00 Sintáxis, variables, tipos de datos y Hola Mundo
+ */
+
+package retosdeprogramaciónmouredev;
+
+public class Alexander {
+
+    public static void main(String[] args) {
+
+        //https://www.java.com/es/
+        
+        //En todos los lenguajes de programación las sintáxis para escribir 
+        //comentarios son dos y son las siguientes
+        
+        //Comentario en una sola línea
+        /*
+        Comentarios multilínea o de varias líneas
+        más lineas de comentarios
+         */
+        
+        int variable = 7;
+        final int variable1 = 45;
+
+        byte valor = 2;
+        short pequeño = 6;
+        int entero = 54;
+        long largo = 5784;
+        float decimal = 0.525f; //necesario la letra f al final del dato 
+                                //para indicar al compilador que es un tipo de dato float
+        double doble = 4.25;
+        boolean booleano = true;
+        char caracter = 'p';
+        
+        System.out.println("Hola, Java!");
+    }
+
+}
